2e9743d8ab9eef2635ff17c272e4e5fadbd23e4d
ch1lly <chillysense@gmail.com>
2023-10-08 14:26:29 +0200

    CORS setup

--- src/main.ts
+++ src/main.ts
@@ -3,12 +3,22 @@ import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  // Create an instance of the Nest application
+  const app = await NestFactory.create(AppModule, { cors: {} });
+
+  // Enable CORS for the application, allowing requests from 'http://localhost:3000' origin
+  app.enableCors({ origin: 'http://localhost:3000', allowedHeaders: '*' });
+
+  // Configure a global validation pipe to whitelist incoming data
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
     }),
   );
+
+  // Start listening on port 4000
   await app.listen(4000);
 }
+
+// Call the bootstrap function to start the application
 bootstrap();
